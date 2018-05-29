f0083349cd15c9a5fcad1b586038cc88e5b93e38
Taydo <leon.marteau.lm@gmail.com>
2018-05-29 20:50:25 +0200

    Multiplication vie ennemi par 10

--- src/EnnemiClassique.cpp
+++ src/EnnemiClassique.cpp
@@ -1,7 +1,7 @@
 #include "EnnemiClassique.h"
 
 EnnemiClassique::EnnemiClassique(int x, int y):
-    Ennemi(x, y, ::textureEnnemiClassique, 15,2,1)
+    Ennemi(x, y, ::textureEnnemiClassique, 150,2,1)
 {
     //ctor
 }
