71af92dbe23050fbb9a03d893fcc2ed75db23635
SergioNbutuna <39455176+SergioNbutuna@users.noreply.github.com>
2018-06-07 19:06:20 +0200

    supression head

--- src/EnnemiVolant.cpp
+++ src/EnnemiVolant.cpp
@@ -1,11 +1,9 @@
 #include "EnnemiVolant.h"
 
 EnnemiVolant::EnnemiVolant(int x, int y, int vague,int xChateau,int yChateau):
-<<<<<<< HEAD
+
     Ennemi(x, y, ::textureEnnemiVolantSimple, 200,2,3, vague, true,xChateau,yChateau)
-=======
-    Ennemi(x, y, ::textureEnnemiVolant, 50,2,3, vague, true,xChateau,yChateau)
->>>>>>> e2f3e4f849eb3496f986a3fa87716d6834877437
+
 {
 
 }
