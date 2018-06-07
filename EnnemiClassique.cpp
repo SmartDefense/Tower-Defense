f0d003ec7c0b60001c313634f7e6c8f4c225f0b2
Taydo <leon.marteau.lm@gmail.com>
2018-06-07 15:37:57 +0200

    Suppression fichier de merde

--- EnnemiClassique.cpp
+++
@@ -1,7 +0,0 @@
-#include "EnnemiClassique.h"
-
-EnnemiClassique::EnnemiClassique(int x, int y, int vague,int xChateau,int yChateau):
-    Ennemi(x, y, ::textureEnnemiClassique, 150,2,1, vague, false,xChateau,yChateau)
-{
-    //ctor
-}
--- EnnemiVolant.cpp
+++
@@ -1,7 +0,0 @@
-#include "EnnemiVolant.h"
-
-EnnemiVolant::EnnemiVolant(int x, int y, int vague,int xChateau,int yChateau):
-    Ennemi(x, y, ::textureEnnemiVolant, 200,2,3, vague, true,xChateau,yChateau)
-{
-
-}
