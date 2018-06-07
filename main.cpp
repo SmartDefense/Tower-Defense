773960dddba9a5696c8c7e8753be5c55ff0b64a2
SergioNbutuna <39455176+SergioNbutuna@users.noreply.github.com>
2018-06-07 15:26:04 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -76,7 +76,7 @@ string nbParties="";                // Nombre de parties du joueur
 string pseudo ="";                  // Variable contenant le pseudo entré par le joueur
 string pseudoCrypte="";
 string pseudoDecrypte="";
-int nbVagues=30;                    // Nombre de vague totale dans chaque partie
+int nbVagues=80;                    // Nombre de vague totale dans chaque partie
 int depart=0;                       // Variable d'état pour commencer à afficher la vague d'ennemis
 int occurences=10;                  // Nombre d'ennemis dans la première vague
 int argent=200;                         // Argent du joueur
@@ -88,13 +88,13 @@ int TAILLE_X_PLATEAU = 20;
 int TAILLE_Y_PLATEAU = 15;
 int nbEnnemisSupplementaires =4;    // Nb d'ennemis supplémentaires à chaque vague
 int compteurImage=0;                // Compteur d'images permettant d'effectuer des actions à un temps donné dans une boucle while
-int rafraichissement=30,
-    xChateau=0,
+int rafraichissement=30;
+
+int xChateau=0,
     yChateau=0;
 
 int xVague=-1,                      // Variables permettant de connaitre la position de la case de départ
     yVague=-1;
-
 
 void quitListeCase()                // Fonction de suppression de la listeCases à la fin de la partie
 {
@@ -381,18 +381,24 @@ int jeu()  // Fonction de gestion et d'affichage de la partie
                 if (vague[numeroEnnemi]==0){
                     listeEnnemis.push_back(new EnnemiRapide(listeCases[1][1]->getXcentre()-TAILLE_ENNEMI/2,
                                                             listeCases[1][1]->getYcentre()-TAILLE_ENNEMI/2,
-                                                            itterations));
+                                                            itterations,
+                                                            xChateau,
+                                                            yChateau));
                 }
                 else if (vague[numeroEnnemi]==1){
                     listeEnnemis.push_back(new EnnemiClassique(listeCases[1][1]->getXcentre()-TAILLE_ENNEMI/2,
                                                                listeCases[1][1]->getYcentre()-TAILLE_ENNEMI/2,
-                                                               itterations));
+                                                               itterations,
+                                                               xChateau,
+                                                               yChateau));
 
                 }
                 else if (vague[numeroEnnemi]==2){
                     listeEnnemis.push_back(new EnnemiTank(listeCases[1][1]->getXcentre()-TAILLE_ENNEMI/2,
                                                           listeCases[1][1]->getYcentre()-TAILLE_ENNEMI/2,
-                                                          itterations));
+                                                          itterations,
+                                                          xChateau,
+                                                          yChateau));
 
                 }
                 numeroEnnemi++;
@@ -438,19 +444,25 @@ int jeu()  // Fonction de gestion et d'affichage de la partie
                 {
                     listeEnnemis.push_back(new EnnemiClassique(listeCases[yCase][xCase]->getXcentre()-TAILLE_ENNEMI/2,
                                            listeCases[yCase][xCase]->getYcentre()-TAILLE_ENNEMI/2,
-                                           itterations));
+                                           itterations,
+                                           xChateau,
+                                           yChateau));
                 }
                 else if(events.key.keysym.scancode==SDL_SCANCODE_E)
                 {
                     listeEnnemis.push_back(new EnnemiRapide(listeCases[yCase][xCase]->getXcentre()-TAILLE_ENNEMI/2,
                                                             listeCases[yCase][xCase]->getYcentre()-TAILLE_ENNEMI/2,
-                                                            itterations));
+                                                            itterations,
+                                                            xChateau,
+                                                            yChateau));
                 }
                 else if(events.key.keysym.scancode==SDL_SCANCODE_Y)
                 {
-                    listeEnnemis.push_back(new EnnemiTank(listeCases[yCase][xCase]->getXcentre()-TAILLE_ENNEMI/2,
+                    listeEnnemis.push_back(new EnnemiVolant(listeCases[yCase][xCase]->getXcentre()-TAILLE_ENNEMI/2,
                                                           listeCases[yCase][xCase]->getYcentre()-TAILLE_ENNEMI/2,
-                                                          itterations));
+                                                          itterations,
+                                                          xChateau,
+                                                          yChateau));
                 }
                 else if(events.key.keysym.scancode==SDL_SCANCODE_1)
                 {
@@ -520,6 +532,8 @@ int jeu()  // Fonction de gestion et d'affichage de la partie
                     else if(events.key.keysym.scancode==SDL_SCANCODE_KP_3)
                     {
                         listeCases[yCase][xCase]=new Chateau(xCase, yCase, 100);
+                        xChateau=xCase;
+                        yChateau=yCase;
                     }
                 }
                 break;
@@ -1361,6 +1375,10 @@ int initSDL()
 
     surface=IMG_Load((CHEMIN_IMAGES+"ennemiTank.png").c_str());
     textureEnnemiTank= SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+    surface=IMG_Load((CHEMIN_IMAGES+"ennemiVolant.png").c_str());
+    textureEnnemiVolant= SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
 
     surface=IMG_Load((CHEMIN_IMAGES+"tirClassique.png").c_str());
